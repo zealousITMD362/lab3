f55dcaea38d1b658be7cc8f7a51e104d24c43f27
zeal0u5 <122125451+zeal0u5@users.noreply.github.com>
2023-10-03 16:15:06 -0500

    added a alert for submitting the email

--- js/site.js
+++ js/site.js
@@ -3,3 +3,8 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+$(document).ready(function () {
+  $("#submit").click(function () {
+    alert("You have been signed up for the Newsletter");
+  });
+});
